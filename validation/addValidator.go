14449bf446c9d1fd9d3059de8d7f0e2681ff35a3
Vyacheslav Utrobin <utrobinslavik@gmail.com>
2023-07-12 04:29:53 +0500

    fix

--- validation/addValidator.go
+++ validation/addValidator.go
@@ -1,6 +1,6 @@
 package validation
 
 type AddRequested struct {
-	Key   string `json:"key" binding:"required"`
-	Value string `json:"value" binding:"required"`
+	Key   string `json:"key" binding:"required,min=3"`
+	Value string `json:"value" binding:"required,min=3"`
 }
